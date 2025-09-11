4d7502f54e452651e3b213616fb3a0ee9826308f
airton646 <airtonlopo26@gmail.com>
2025-09-11 19:55:01 -0300

    AlunoRepository.java

--- src/br/com/escola/projeto/repositories/AlunoRepository.java
+++ src/br/com/escola/projeto/repositories/AlunoRepository.java
@@ -1,1 +1,14 @@
-.
+package br.com.escola.projeto.repositories;
+
+import br.com.escola.projeto.models.Aluno;
+import java.util.List;
+import java.util.Optional;
+
+public interface AlunoRepository {
+ void salvar(Aluno aluno);
+ Optional<Aluno> buscarPorId(int id);
+ Optional<Aluno> buscarPorMatricula(String matricula);
+ List<Aluno> listarTodos();
+ void atualizar(Aluno aluno);
+ boolean remover(int id);
+}
